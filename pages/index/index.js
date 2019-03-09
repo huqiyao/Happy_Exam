b866b4a8e8675f98498998c13da48dfda93d2677
AtongSF <AtongSF@gmail.com>
2019-03-09 20:15:58 +0800

    首页完善样式+canvas绘进度条

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,51 @@
 //index.js
 var app = getApp();
 var globalData = app.globalData;
+var context = new wx.createCanvasContext('canvasArc', this);
+var back = new wx.createCanvasContext('canvasBack')
+var strat_num = 1, end_num = 20;
+var sAngle = 1.5 * Math.PI, eAngle = 0;
 Page({
   data: {
     exerciseType: ["随机练习", "专项练习", "我的收藏", "我的错题"],
     show: false, //控制下拉列表的显示隐藏，false隐藏、true显示
     selectData: ['语文', '数学', '英语', '科学', '思品', '化学', '生物'], //下拉列表的数据
     index: 0 //选择的下拉列表下标
+  },
+  /**
+   * 进度条
+   */
+  onReady: function () {
+    // 画背景圆
+    back.setStrokeStyle("#eaeaea")
+    back.setLineWidth(5)
+    back.setLineCap('round');
+    back.beginPath();
+    back.arc(41, 43, 38, 0,2 * Math.PI, false)
+    back.stroke()
+    back.draw()
+    // 画动态进度条
+    this.canvas()
+  },
+  canvas: function () {
+    var that = this;
+    if (strat_num <= end_num) {
+      console.log('strat_num:', strat_num)
+      eAngle = strat_num * 2 * Math.PI / end_num + 1.5 * Math.PI;
+      setTimeout(function () {
+        context.setStrokeStyle("#41C7DB")
+        context.setLineWidth(5)
+        context.fillText(strat_num * 5 <= 100 ? strat_num * 5 + '%' : 100 + '%', 25, 48)
+        context.setFontSize(15)
+        context.arc(41, 43, 38, sAngle, eAngle, false)
+        context.stroke()
+        context.draw()
+        that.canvas()
+        strat_num++
+      }, 200)
+    } else {
+      console.log('strat_num_end:', strat_num)
+    }
   },
   /**
    * 选择练习科目
@@ -32,8 +71,6 @@ Page({
     // globalData.currentExerciseType = e.currentTarget.dataset.type
     var type = e.currentTarget.dataset.type
     wx.navigateTo({
-    //   url: '',
-    // })({
       url: '/pages/exercise/exercise?currentExerciseType=' + type,
       // success: function (e) {  //切换过去要刷新
       //   var page = getCurrentPages().pop();
